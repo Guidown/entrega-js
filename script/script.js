c7ed5dcbd3804cc39e1aeb634b494ce6eeb1ae06
Guido <guidobombini@gmail.com>
2022-08-18 21:48:13 -0300

    2do commit

--- script/script.js
+++ script/script.js
@@ -1,132 +1,149 @@
-let titulo = document.getElementById("titulo");
-titulo.style.textAlign = "center";
-titulo.style.margin = "30px";
-titulo.style.color = "white";
-titulo.style.backgroundColor = "RoyalBlue";
-titulo.style.padding = "30px";
+function cargarProductos() {
+  productos.push(
+    new Producto(1, "Samsung Galaxy S10", 66800, "./Imgs/Samsung-S10.png")
+  );
+  productos.push(
+    new Producto(2, "Samsung Galaxy S20", 120000, "./Imgs/Samsung-S20.png")
+  );
+  productos.push(
+    new Producto(3, "Samsung Galaxy S21", 146200, "./Imgs/Samsung-S21.png")
+  );
+  productos.push(
+    new Producto(
+      4,
+      "Samsung Galaxy S22 Ultra",
+      198046,
+      "./Imgs/Samsung-Galaxy-S22-Ultra.png"
+    )
+  );
+  productos.push(new Producto(5, "Iphone 13", 190970, "./Imgs/iPhone-13.png"));
+  productos.push(
+    new Producto(6, "Iphone 13 Mini", 243923, "./Imgs/iphone-13-mini.png")
+  );
+  productos.push(
+    new Producto(7, "Iphone 13 Pro", 268250, "./Imgs/iphone-13-Pro.png")
+  );
+  productos.push(
+    new Producto(8, "Iphone 13 Pro Max", 295164, "./Imgs/iphone-13-pro-max.png")
+  );
+  productos.push(
+    new Producto(9, "Huawei P30 Lite", 93606, "./Imgs/huawei-p30-lite.png")
+  );
+  productos.push(
+    new Producto(10, "Huawei P40 Pro", 108606, "./Imgs/huawei-p40-pro.png")
+  );
+  productos.push(
+    new Producto(
+      11,
+      "Huawei Mate 30 Pro",
+      146024,
+      "./Imgs/huawei-mate30-pro.png"
+    )
+  );
+  productos.push(
+    new Producto(12, "Huawei Mate 30", 102300, "./Imgs/huawei-mate-30.png")
+  );
+}
 
-let abajo = document.getElementById("abajo");
-abajo.style.textAlign = "center";
-abajo.style.color = "RoyalBlue";
+class Producto {
+  constructor(id, nombre, precio, foto) {
+    this.id = id;
+    this.nombre = nombre;
+    this.precio = precio;
+    this.foto = foto;
+  }
+}
 
-let principal = document.getElementById("principal");
-principal.style.margin = "30px";
+class ElementoCarrito {
+  constructor(producto, cantidad) {
+    this.producto = producto;
+    this.cantidad = cantidad;
+  }
+}
 
-/*let fin = document.getElementById("fin");
-fin.style.margin = "30px";
-fin.style.padding = "20px";*/
+const productos = [];
+const elementosCarrito = [];
 
-const carrito = [];
+const contenedorProductos = document.getElementById("contenedor-productos");
+const contenedorCarritoCompras = document.querySelector("#items");
+const contenedorFooterCarrito = document.querySelector("#footer");
 
-const productos = [
-  {
-    id: 1,
-    nombre: "Samsung Galaxy S10",
-    precio: 66800,
-    foto: "./Imgs/Samsung-S10.png",
-  },
-  {
-    id: 2,
-    nombre: "Samsung Galaxy S20",
-    precio: 120000,
-    foto: "./Imgs/Samsung-S20.png",
-  },
-  {
-    id: 3,
-    nombre: "Samsung Galaxy S21",
-    precio: 146200,
-    foto: "./Imgs/Samsung-S21.png",
-  },
-  {
-    id: 4,
-    nombre: "Samsung Galaxy S22 Ultra",
-    precio: 198046,
-    foto: "./Imgs/Samsung-Galaxy-S22-Ultra.png",
-  },
-  {
-    id: 5,
-    nombre: "Iphone 13 ",
-    precio: 190970,
-    foto: "./Imgs/iPhone-13.png",
-  },
-  {
-    id: 6,
-    nombre: "Iphone 13 Mini",
-    precio: 243923,
-    foto: "./Imgs/iphone-13-mini.png",
-  },
-  {
-    id: 7,
-    nombre: "Iphone 13 Pro",
-    precio: 268250,
-    foto: "./Imgs/iphone-13-Pro.png",
-  },
-  {
-    id: 8,
-    nombre: "Iphone 13 Pro Max",
-    precio: 295164,
-    foto: "./Imgs/iphone-13-pro-max.png",
-  },
-  {
-    id: 9,
-    nombre: "Huawei P30 Lite",
-    precio: 93606,
-    foto: "./Imgs/huawei-p30-lite.png",
-  },
-  {
-    id: 10,
-    nombre: "Huawei P40 Pro",
-    precio: 108606,
-    foto: "./Imgs/huawei-p40-pro.png",
-  },
-  {
-    id: 11,
-    nombre: "Huawei Mate 30 Pro",
-    precio: 146024,
-    foto: "./Imgs/huawei-mate30-pro.png",
-  },
-  {
-    id: 12,
-    nombre: "Huawei Mate 30",
-    precio: 123024,
-    foto: "./Imgs/huawei-mate-30.png",
-  },
-];
+cargarProductos();
+catalogoProductos();
 
-let cartas = document.getElementById("cartas");
-for (const producto of productos) {
-  let carta = document.createElement("div");
-  carta.className = "card col-md-3";
-  carta.innerHTML = `
-        <img src="${producto.foto}" class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">${producto.nombre}</h5>
-            <p class="card-text">${producto.precio}</p>
-            <button id="miBoton--${producto.nombre}" class="btn btn-primary">COMPRAR</button>
-        </div>
-    `;
-  cartas.append(carta);
+function catalogoProductos() {
+  contenedorProductos.innerHTML = "";
 
-  let miBoton = document.getElementById(`miBoton--${producto.nombre}`);
-
-  miBoton.addEventListener("click", (e) => {
-    e.preventDefault();
-    console.log("agregaste esta bebida al carrito");
-    carrito.push(producto);
+  productos.forEach((producto) => {
+    let carta = crearCard(producto);
+    contenedorProductos.append(carta);
   });
 }
 
-let botonFinDeCompra = document.getElementById("fin");
-let precioFinal = 0;
-let checkOut = 0;
-botonFinDeCompra.addEventListener("click", (e) => {
-  e.preventDefault();
-  precioFinal = carrito.map((carrito) => carrito.precio);
-  checkOut = precioFinal.reduce((ac, el) => ac + el, 0);
-  console.table(carrito);
-  const filtro = carrito.filter((pro) => pro.celulares == true);
-  console.log("Los siguientes son telefonos");
-  console.log(filtro);
+function crearCard(producto) {
+  let botonAgregar = document.createElement("button");
+  botonAgregar.className = "btn btn-dark";
+  botonAgregar.innerText = "Agregar";
 
-  console.log("El total a pagar es $" + checkOut);
-});
+  let cuerpoCarta = document.createElement("div");
+  cuerpoCarta.className = "card-body";
+  cuerpoCarta.innerHTML = `
+        <h5>${producto.nombre}</h5>
+        <p>$ ${producto.precio} $</p>
+    `;
+  cuerpoCarta.append(botonAgregar);
+
+  let imagen = document.createElement("img");
+  imagen.src = producto.foto;
+  imagen.className = "card-img-top";
+  imagen.alt = producto.nombre;
+
+  let carta = document.createElement("div");
+  carta.className = "card m-2 p-2";
+  carta.style = "width: 18rem";
+  carta.append(imagen);
+  carta.append(cuerpoCarta);
+
+  botonAgregar.onclick = (e) => {
+    let elementoCarrito = new ElementoCarrito(producto, 1);
+    elementosCarrito.push(elementoCarrito);
+
+    dibujarCarrito();
+  };
+
+  return carta;
+}
+
+function dibujarCarrito() {
+  contenedorCarritoCompras.innerHTML = "";
+
+  let totalCarrito = 0;
+
+  elementosCarrito.forEach((elemento) => {
+    let renglonCarrito = document.createElement("tr");
+
+    renglonCarrito.innerHTML = `
+                <td>${elemento.producto.id}</td>
+                <td>${elemento.producto.nombre}</td>
+                <td><input id="cantidad-producto-${
+                  elemento.producto.id
+                }" type="number" value="${
+      elemento.cantidad
+    }" min="1" max="1000" step="1" style="width: 50px;"/></td>
+                <td>${elemento.producto.precio}</td>
+                <td>${elemento.producto.precio * elemento.cantidad}</td>
+            `;
+    totalCarrito += elemento.producto.precio * elemento.cantidad;
+    contenedorCarritoCompras.append(renglonCarrito);
+
+    let inputCantidad = document.getElementById(
+      `cantidad-producto-${elemento.producto.id}`
+    );
+
+    inputCantidad.addEventListener("change", (e) => {
+      let nuevaCantidad = e.target.value;
+      elemento.cantidad = nuevaCantidad;
+      dibujarCarrito();
+    });
+  });
+}
